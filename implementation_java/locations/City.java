fe880e2c8e25347b669052a3966f90def55f99d9
AsifAliKhan2001 <156850926+AsifAliKhan2001@users.noreply.github.com>
2024-10-18 16:56:55 -0400

    Create City.java

---
+++ implementation_java/locations/City.java
@@ -0,0 +1,20 @@
+public class City {
+    // Attributes
+    private String name;
+    private String province;
+
+    // Constructor
+    public City(String name, String province) {
+        this.name = name;
+        this.province = province;
+    }
+
+    // Methods
+    public String getName() {
+        return name;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+}
