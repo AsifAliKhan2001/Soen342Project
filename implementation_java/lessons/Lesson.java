de0c2bbfeb0854d2ba4ff714c43172ea3c1ba8c7
AsifAliKhan2001 <156850926+AsifAliKhan2001@users.noreply.github.com>
2024-10-18 16:53:03 -0400

    Create Lesson.java

---
+++ implementation_java/lessons/Lesson.java
@@ -0,0 +1,14 @@
+public class Lesson {
+    // Attributes
+    private String type; // e.g., swimming, judo
+
+    // Constructor
+    public Lesson(String type) {
+        this.type = type;
+    }
+
+    // Methods
+    public String getType() {
+        return type;
+    }
+}
